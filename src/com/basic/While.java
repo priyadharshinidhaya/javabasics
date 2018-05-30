5ea2547b32483e05784f5608aa4df6a157006203
priyadharshinidhaya <priyadharshiniksr.cse@gmail.com>
2018-05-30 10:53:47 +0530

    Error identified:local variable should be initialized

---
+++ src/com/basic/While.java
@@ -0,0 +1,17 @@
+package com.basic;
+
+public class While {
+	public static void main(String[] args)
+	{
+		int x=1,total=0;
+		while ( x <= 10 )
+		{
+		total += x;
+		++x;
+		System.out.println(total);
+		}
+
+	
+	}
+
+}
