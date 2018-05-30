a4ea75c166e870bb1f19851ad6bee2e3445e61af
priyadharshinidhaya <priyadharshiniksr.cse@gmail.com>
2018-05-30 09:32:37 +0530

    Data type example

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class DataTypeExample {
+	public static void main(String a[]) {
+byte one=100;
+byte two=2;
+byte three=(byte)(one + two);
+System.out.println(three);
+}
+}
