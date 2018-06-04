8df3565dc508d5ff4f7d2d4f3316380e2f43ca8e
priyadharshinidhaya <priyadharshiniksr.cse@gmail.com>
2018-06-04 09:48:55 +0530

    Array-  Program to find minimum and maximum value

---
+++ src/com/basic/Array1.java
@@ -0,0 +1,34 @@
+package com.basic;
+
+public class Array1 {
+	int arr[] = { 5, 3, 2, 7, 8, 0, 67 };
+	int i, max, min;
+
+	public void maxArray() {
+		max = arr[0];
+		for (i = 0; i < arr.length; i++) {
+			if (max < arr[i]) {
+				max = arr[i];
+			}
+        }
+		System.out.println("Maximum is" + max);
+
+	}
+
+	public void minArray() {
+		min = arr[0];
+		for (i = 0; i < arr.length; i++) {
+			if (min > arr[i]) {
+				min = arr[i];
+			}
+
+		}
+		System.out.println("Minimum is" + min);
+	}
+
+	public static void main(String args[]) {
+		Array1 arr = new Array1();
+		arr.maxArray();
+		arr.minArray();
+	}
+}
